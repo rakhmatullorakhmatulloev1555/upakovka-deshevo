b7edce6a0cc4f5a5c9809ad08de1eeea7a3fa768
Rakhmatullo <65566629+rakhmatullorakhmatulloev1555@users.noreply.github.com>
2023-06-14 17:43:26 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Swiper
+
+ $(".carousel").owlCarousel({
+  loop: true,
+  responsive:{
+    0:{
+      items: 1,
+      nav: false,
+    },
+    600:{
+      items: 2,
+      nav: false,
+    },
+    1000:{
+      items: 3,
+      nav: false,
+    },
+  }
+ });
